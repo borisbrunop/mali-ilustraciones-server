90d38ccc50c04698b5503bef49eee74dadabf510
Boris Bruno <boris.bruno@phinxlab.com>
2022-02-10 17:12:45 -0400

    change cors allow origin

--- build/index.js
+++ build/index.js
@@ -37,8 +37,26 @@ const notion_1 = __importStar(require("./services/notion"));
 const findPageName_1 = __importDefault(require("./utils/findPageName"));
 const databases_1 = require("./const/databases");
 var cors = require('cors');
+var allowedOrigins = ['http://localhost:3000',
+    'https://mali-ilustraciones.web.app/'];
 const app = (0, express_1.default)();
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin)
+            return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }));
 // var databases = require('./const/databases');
 const port = process.env.PORT || 5400;
 // app.get('/all', async (req, res) => {
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import express  from "express";
+import express from "express";
 import 'dotenv/config'
 import get, { getComponents } from './services/notion'
 import findPageName from './utils/findPageName'
-import {databases} from './const/databases'
+import { databases } from './const/databases'
 var cors = require('cors');
 
+var allowedOrigins = ['http://localhost:3000',
+    'https://mali-ilustraciones.web.app/'];
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: function (origin:any, callback:any) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}))
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }));
 
 // var databases = require('./const/databases');
 const port = process.env.PORT || 5400
@@ -26,22 +43,22 @@ const port = process.env.PORT || 5400
 // })
 
 app.get('/:name', async (req, res) => {
-    try{
+    try {
         const response = await get(req.params.name, 'POST');
         const name = findPageName(req.params.name)
         console.info(`get database ${name} 200 response status`)
         res.json(response)
-    }catch(e){
+    } catch (e) {
         console.error(e)
     }
 })
 app.get(`/${databases.PHOTOS}/:key`, async (req, res) => {
-    try{
+    try {
         const response = await getComponents(databases.PHOTOS, 'POST', req.params.key);
         const name = findPageName(databases.PHOTOS)
         console.info(`get database ${name} ${req.params.key} 200 response status`)
         res.json(response)
-    }catch(e){
+    } catch (e) {
         console.error(e)
     }
 })
