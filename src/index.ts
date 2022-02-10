3834846af8acaf1926fad7cbf1d0f75fb39da29f
Boris Bruno <boris.bruno@phinxlab.com>
2022-02-10 17:32:27 -0400

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,8 @@ var allowedOrigins = ['http://localhost:3000',
 
 
 const app = express();
-app.use(cors({
-    origin: function (origin:any, callback:any) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}))
-// app.use(cors({
-//     origin: 'http://localhost:3000'
-// }));
+app.use(cors())
 
-// var databases = require('./const/databases');
 const port = process.env.PORT || 5400
 
 // app.get('/all', async (req, res) => {
